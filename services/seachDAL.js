48ed3b24d68e743d0bc63ba1ab9f5436d903fb33
Nathan Greene <nathan.greene@keyin.com>
2023-04-11 13:30:20 -0230

    Logevents.js, foote.ejs & head.ejs
    1. Added logevents for the events we have.
    
    2. Added a copywright
    
    3. Changed the nav.ejs title and added it to all pages

--- services/seachDAL.js
+++ services/seachDAL.js
@@ -1,6 +1,14 @@
 const dal = require("./auth_PG_DB");
 const mDal = require("./mdb");
 
+const logEvents = require("../logevents");
+const EventEmitter = require("events");
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on("log", (event, level, msg) => logEvents(event, level, msg));
+
 var searchDatabase = function (keyword, database) {
   if (DEBUG) console.log("logins.pg.dal.getLoginByLoginId()");
 
@@ -8,9 +16,21 @@ var searchDatabase = function (keyword, database) {
     const sql = `SELECT * FROM students WHERE CAST (id AS TEXT) LIKE $1 OR first_name LIKE $1 OR last_name LIKE $1 OR email LIKE $1 OR gender LIKE $1 OR CAST (age AS TEXT) LIKE $1 OR CAST (semester AS TEXT) LIKE $1`;
     dal.query(sql, [`%${keyword}%`], (err, result) => {
       if (err) {
+        myEmitter.emit(
+          "log",
+          "searhDatabase",
+          "ERROR",
+          "searchDatabase has Failed!(PSQL)"
+        );
         if (DEBUG) console.log(err);
         reject(err);
       } else {
+        myEmitter.emit(
+          "log",
+          "searhDatabase",
+          "INFO",
+          "searchDatabase was Successful!(PSQL)"
+        );
         resolve(result.rows);
       }
     });
